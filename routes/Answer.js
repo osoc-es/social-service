bfff6675f3a6dbf58cc64476cc3a2f5f9933a755
Muhammad Laiq <ujankhan66@gmail.com>
2019-07-11 12:28:35 +0200

    updated form submission

--- routes/Answer.js
+++ routes/Answer.js
@@ -10,29 +10,20 @@ const db = require('../database/db.js')
 const fs = require('fs');
 const Json2csvParser = require('json2csv').Parser;
 
-answers.post('/submit/', (req, res) => {
-    var data;
+answers.post('/submit/', (req, res) => {  
+    if(req.body.data!=null){
     for(i in req.body.data){
         var formData=req.body.data;
         var data={
             Answer:formData[i].Answer,
-            AnswerType:formData[i].AnswerType,
-            Email:formData[i].Email,
-            QuestionId:formData[i].QuestionId
-        }
-     }   
-     res.status(200).send(data);
-    /* if(req.body.Answer!="" & req.body.Answer!=null & req.body.AnswerType!="" & req.body.AnswerType!=null){
-        var data={
-            Answer:req.body.Answer,
-            AnswerType:req.body.AnswerType
+            AnswerType:formData[i].AnswerType
         }
         Answer.create(data)
          .then(function(result){
              if(result){
                 UserAnswer.create({
-                 Email:req.params.Email,
-                 QuestionId:req.params.QuestionId,
+                 Email:formData[i].Email,
+                 QuestionId:formData[i].QuestionId,
                  AnswerId:result.AnswerId
                 }).then(function(result){
                     res.status(200).json("Answer Submitted Sucessfully..!!")
@@ -45,10 +36,11 @@ answers.post('/submit/', (req, res) => {
          .catch(err => {
             res.status(400).json('error: ' + err)
           })
+        }
     }else{
         res.status(400).send("Invlaid data.")
     }
-  */
+  
 });
 //get submitted answers
 answers.get('/:Email/:title', (req, res) => {
